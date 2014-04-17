2d51548e65a6bdd9fc61628febe8d0aa59f5041f
Adam Thorson <elquintoviento@gmail.com>
2014-04-17 16:25:06 -0500

    Initial Commit

---
+++ AccelerationAndRotationTutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AccelerationAndRotationTutorial
+//
+//  Created by Adam Thorson on 4/17/14.
+//  Copyright (c) 2014 Adam Thorson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ AccelerationAndRotationTutorialTests/AccelerationAndRotationTutorialTests.m
@@ -0,0 +1,34 @@
+//
+//  AccelerationAndRotationTutorialTests.m
+//  AccelerationAndRotationTutorialTests
+//
+//  Created by Adam Thorson on 4/17/14.
+//  Copyright (c) 2014 Adam Thorson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AccelerationAndRotationTutorialTests : XCTestCase
+
+@end
+
+@implementation AccelerationAndRotationTutorialTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
